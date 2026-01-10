f5a566163c11b875e802e5a69ae829ca6f9995d5
Ivan Ivanov <ivanov.ivan.v@gmail.com>
2026-01-10 16:49:27 +0200

    fetch url - implement feature to check whether http:// is present and add it if not - implement possibility to display response status from fetching the url

--- chap_1/fetch_url.go
+++ chap_1/fetch_url.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"strings"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -8,9 +9,19 @@ import (
 	"io"
 )
 
+const (
+	HTTP_PREFIX = "http://"
+)
+
 func main() {
 	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, HTTP_PREFIX) {
+			url = HTTP_PREFIX + url
+			fmt.Printf("\nurl is now: %s\n", url)
+		}
+
 		resp, err := http.Get(url)
+		fmt.Printf("\n\n\nResponse: %s\n\n\n", resp.Status)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
@@ -20,8 +31,6 @@ func main() {
 			fmt.Fprintf(os.Stderr, "error copying body %v", err)
 			os.Exit(1)
 		}
-
-		fmt.Printf("%s", readAll(resp, url))
 	}
 }
 
